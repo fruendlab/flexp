f73a52f88bfbd71cfd963e90bc317519c486ef58
Ingo Fruend <mail@ingofruend.net>
2019-09-12 10:23:45 -0400

    initial display stuff

---
+++ src/main/python/flexp/display.py
@@ -0,0 +1,28 @@
+# Display++ abstraction
+from psychopy.monitors import Monitor
+from psychopy.visual import Window
+from psychopy.hardware.crs.bits import DisplayPlusPlus
+
+
+class dppWindow(Window):
+
+    def __init__(self, mode="mono++", **kwargs):
+        self.monitor = Monitor('Display++',
+                               width=80,
+                               distance=57)
+        self.monitor.setSizePix((1920, 1080))
+
+        portName = kwargs.pop('portName', '/dev/ttyACM0')
+        gamma = kwargs.pop('gamma', 'hardware')
+
+        kwargs.setdefault('screen', 1)
+        kwargs['useFBO'] = True
+        kwargs['size'] = (1920, 1080)
+        kwargs['monitor'] = self.monitor
+        super(DisplayPlusPlus, self).__init__(**kwargs)
+
+        self.dpp = DisplayPlusPlus(self.win,
+                                   portName=portName,
+                                   gamma=gamma,
+                                   mode=mode,
+                                   checkConfigLevel=1)
