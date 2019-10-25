0d18384524604880dafd52f9b7b74cccacaf5953
Ingo Fruend <mail@ingofruend.net>
2019-10-25 11:48:23 -0400

    more tests

--- src/unittest/python/experiment_tests.py
+++ src/unittest/python/experiment_tests.py
@@ -9,7 +9,7 @@ class TestInitialization(TestCase):
     def setUp(self):
         self.patches = {
             'window': mock.patch('psychopy.visual.Window'),
-            'dpp': mock.patch('flexp.display.dppWindow'),
+            'dpp': mock.patch('flexp.experiment.dppWindow'),
             'circle': mock.patch('psychopy.visual.Circle'),
             'textstim': mock.patch('psychopy.visual.TextStim'),
             'sound': mock.patch('flexp.experiment.Sound'),
@@ -25,4 +25,27 @@ class TestInitialization(TestCase):
     def test_develop_true_creates_standard_window(self):
         expr = BaseExperiment(develop=True)
         self.mocks['window'].assert_called_once_with(winType='pygame')
+        self.mocks['dpp'].assert_not_called()
         self.assertEqual(expr.win, self.mocks['window'].return_value)
+
+    def test_develop_true_can_still_overwrite_wintype(self):
+        expr = BaseExperiment(develop=True, winType='pyglet')
+        self.mocks['window'].assert_called_once_with(winType='pyglet')
+        self.mocks['dpp'].assert_not_called()
+        self.assertEqual(expr.win, self.mocks['window'].return_value)
+
+    def test_develop_false_creates_dpp(self):
+        expr = BaseExperiment(develop=False)
+        self.mocks['window'].assert_not_called()
+        self.mocks['dpp'].assert_called_once_with()
+        self.assertEqual(expr.win, self.mocks['dpp'].return_value)
+
+    def test_additional_kwds_are_passed_through(self):
+        for key, develop in [('window', True), ('dpp', False)]:
+            with self.subTest(develop=develop):
+                BaseExperiment(develop=develop,
+                               winType='ANY_WINTYPE',  # for develop=True
+                               any_keyword='ANY_VALUE')
+                self.mocks[key].assert_called_once_with(
+                    winType='ANY_WINTYPE',
+                    any_keyword='ANY_VALUE')
